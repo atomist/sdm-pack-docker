c70bfff763492ba620d136121880199d6c0f474e
Rod Johnson <rod@atomist.com>
2019-07-03 13:41:47 -0700

    Make parsing more forgiving

--- test/parse/dockerFileParser.test.ts
+++ test/parse/dockerFileParser.test.ts
@@ -203,6 +203,16 @@ EXPOSE 8080`;
         assert.strictEqual(contentNow, nodeDockerfile.replace("argon", "xenon"));
     });
 
+    it("should parse problematic file", async () => {
+        const p = InMemoryProject.of(
+            {path: "Dockerfile", content: weave1},
+        );
+        const images = await astUtils.findValues(p, DockerFileParser, "Dockerfile",
+            "//FROM/image");
+        assert.strictEqual(images.length, 1);
+        assert.strictEqual(images[0], "weaveworksdemos/msd-java:jre-latest");
+    });
+
 });
 
 const nodeDockerfile = `FROM node:argon
@@ -255,3 +265,28 @@ FROM thing
 
 EXPOSE 8080
 `;
+
+const weave1 = `FROM weaveworksdemos/msd-java:jre-latest
+
+WORKDIR /usr/src/app
+COPY *.jar ./app.jar
+
+RUN	chown -R \${SERVICE_USER}:\${SERVICE_GROUP} ./app.jar
+
+USER \${SERVICE_USER}
+
+ARG BUILD_DATE
+ARG BUILD_VERSION
+ARG COMMIT
+
+LABEL org.label-schema.vendor="Weaveworks" \\
+  org.label-schema.build-date="\${BUILD_DATE}" \\
+  org.label-schema.version="\${BUILD_VERSION}" \\
+  org.label-schema.name="Socks Shop: Cart" \\
+  org.label-schema.description="REST API for Cart service" \\
+  org.label-schema.url="https://github.com/microservices-demo/carts" \\
+  org.label-schema.vcs-url="github.com:microservices-demo/carts.git" \\
+  org.label-schema.vcs-ref="\${COMMIT}" \\
+  org.label-schema.schema-version="1.0"
+
+ENTRYPOINT ["/usr/local/bin/java.sh","-jar","./app.jar", "--port=80"]`;
